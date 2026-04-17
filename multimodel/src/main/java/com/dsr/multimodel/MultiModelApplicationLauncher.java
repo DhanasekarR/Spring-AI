cb720d4ac9284f47a31a3ebd2802c505eecce14d
Dhanasekar Ramachandra <dhanasekarit@gmail.com>
2026-04-17 22:59:36 +1000

    MultiModel setup

---
+++ multimodel/src/main/java/com/dsr/multimodel/MultiModelApplicationLauncher.java
@@ -0,0 +1,11 @@
+package com.dsr.multimodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModelApplicationLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModelApplicationLauncher.class);
+    }
+}
